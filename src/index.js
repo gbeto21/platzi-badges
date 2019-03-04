5f1ca6e5c0b325be65cb28b445fa08b9049b1e2a
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-03-04 10:05:33 -0500

    1. ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// const element = document.createElement('h1')
+// element.innerText = 'Hello, Platzi Badges!'
 
-const container = document.getElementById('app');
+// const container = document.getElementById('app')
 
-const element = <h1>Hello, Platzi Badges</h1>;
+// container.appendChild(element)
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>Hello, Platzy Badges from React!</h1>;
+const container = document.getElementById("app");
+
+//ReactDOM el qué y en dónde.
+//Es necesario importar Reac en dónde se utilize JSX
 ReactDOM.render(element, container);
