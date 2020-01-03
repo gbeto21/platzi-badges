7f680669b5de6c4fe7033ba4ba34b1a527535907
Alberto González Balderas <albertoglezb@hotmail.com>
2020-01-02 20:15:09 -0600

    Se definió el término de JSX y se terminó la clase: 'JSX'.

--- src/index.js
+++ src/index.js
@@ -8,9 +8,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = <h1>Hello, Platzy Badges from React!</h1>;
+const element = React.createElement(
+  "a",
+  { href: "https://platzi.com" },
+  "Ir a platzi"
+);
+
+const name = "Ricardoman";
+const element2 = React.createElement("h1", {}, `Hola soy ${name}`);
+const sum = () => 3 + 3;
+const element3 = <h1>Hola soy, {undefined}</h1>;
+const element4 = (
+  <div>
+    <h1>Hola, Soy Richard</h1>
+    <p>Soy software developer. </p>
+  </div>
+);
 const container = document.getElementById("app");
 
 //ReactDOM el qué y en dónde.
 //Es necesario importar Reac en dónde se utilize JSX
-ReactDOM.render(element, container);
+ReactDOM.render(element4, container);
